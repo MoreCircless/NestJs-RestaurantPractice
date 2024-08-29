efededb67cca15afd31040fff9577960b946a771
pablo <pablo@pablo.pablo>
2024-08-29 19:35:41 +0200

    fixed bug on prismaService and auth was added as a new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, AuthModule],
   controllers: [],
   providers: [],
 })
